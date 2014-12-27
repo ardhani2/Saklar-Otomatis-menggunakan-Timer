e732432080b6f73e529fb3ee608b8101c6469be4
ardhani2 <ardhani2@yahoo.com>
2014-12-26 20:09:28 -0800

    Update Saklar Otomatis menggunakan Timer.c

--- Saklar Otomatis menggunakan Timer.c
+++ Saklar Otomatis menggunakan Timer.c
@@ -1,9 +1,3 @@
-/*Saklar-Otomatis-menggunakan-Timer
-=================================
-
-Kodingan saklar timer pada atmega 8535 menggunakan cvavr2
-*/
-
 /*****************************************************
 This program was produced by the
 CodeWizardAVR V2.05.3 Standard
@@ -13,12 +7,12 @@ http://www.hpinfotech.com
 
 Project : 
 Version : 
-Date    : 12/26/2014
+Date    : 12/24/2014
 Author  : Dhani
 Company : 
 Comments: 
 
-                                          a
+
 Chip type               : ATmega8535
 Program type            : Application
 AVR Core Clock frequency: 11.059200 MHz
@@ -28,18 +22,242 @@ Data Stack size         : 128
 *****************************************************/
 
 #include <mega8535.h>
+#include <delay.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // Alphanumeric LCD functions
 #include <alcd.h>
+
+//---------------------------Variabel Global----------------------------------//
+
+eeprom int d=0,m=0,j=0; // disimpan di eeprom agar tidak terhapus bila mati 
+unsigned int cekTimer=0,detik=0,menit=0,jam=0,flagMenu=0;
+unsigned char cdetik[10],cmenit[10],cjam[10];
+
+//----------------------------- display buat led -----------------------------//
+ void tampilan(){           
+            lcd_gotoxy(9,0);      // posisi pointer pada LCD
+            lcd_putsf(":");       // display nilai ascii-nya
+            lcd_gotoxy(10,0);
+            lcd_puts(cdetik);
+            lcd_gotoxy(6,0);
+            lcd_putsf(":");
+            lcd_gotoxy(7,0);
+            lcd_puts(cmenit);
+            lcd_gotoxy(5,0);
+            lcd_puts(cjam);
+ }
+
+// fungsi untuk merubah integer to ascii untuk didisplay ke LCD            
+//------------------------------ ITOA variabel set ------------------------//
+// variabel untuk display saat set timer
+void itoaVarSet(){       
+    itoa(d,cdetik); 
+    itoa(m,cmenit);
+    itoa(j,cjam);
+}
+
+//------------------------------ ITOA Variabel Count ----------------------//
+// variabel 
+void itoaVarCount(){
+    itoa(detik,cdetik); 
+    itoa(menit,cmenit);
+    itoa(jam,cjam);
+}
+//-------------------------------Menu-------------------------------------//
+
+void menu(){
+
+
+// konfigurasi tombol b0=inc input, b1=dec input, b2=next setting, b3= COUNTDOWN MULAI
+
+    //setTimer:
+    if(flagMenu==0){        // saat flag menu = 0
+        PORTA.0=0;          // mematikan sinyal pada pin a.0
+        lcd_clear();
+        lcd_gotoxy(0,0);
+        lcd_putsf("Set Timer");   // menampilkan menu setting timer
+        delay_ms(1000);
+        lcd_clear();
+        flagMenu=1;          // set flag menu menuju setting detik
+    }
+       
+    //set_detik:
+    while(flagMenu==1){
+        lcd_clear();
+        itoaVarSet();
+        tampilan();           // memanggil fungsi tampilan untuk menampilkan nilai yang akan di countdown
+        lcd_gotoxy(0,1);
+        lcd_putsf("set detik");
+        delay_ms(100);
+        if(PINB.0==0){            // ketika pin b.0 ditekan logika nol active low, rangkaian berupa pull up resistor
+            delay_ms(150);        // delay diberikan agar tombol memberikan nilai yang tidak kelewatan banyak ketika ditekan
+            d=d+1;                // increment d
+        }
+        if(PINB.1==0){
+            delay_ms(150);
+            d=d-1;               // decrement
+        }
+        if(PINB.2==0){
+            delay_ms(150);
+            flagMenu=2;          // flag menandakan setting menit
+            break;
+            //goto set_menit;
+        }
+        if(PINB.3==0){
+            delay_ms(150);
+            flagMenu=4;          // flag menandakan hitung mundur dimulai
+            break;
+            //goto countDown;
+        }
+        if(d==-1){
+            d=0;                 // menset nilai d nol ketika mencapai minus satu
+        }
+    } 
+     
+//set_menit:
+    while(flagMenu==2){
+        lcd_clear();
+        itoaVarSet();
+        tampilan();
+        lcd_gotoxy(0,1);
+        lcd_putsf("set menit");
+        delay_ms(100);
+        if(PINB.0==0){
+            delay_ms(150);
+            m=m+1;
+        }
+        if(PINB.1==0){
+            delay_ms(150);
+            m=m-1;
+        }
+        if(PINB.2==0){
+            delay_ms(150);
+            flagMenu=3;
+            break;
+            //goto set_jam;
+        }
+        if(PINB.3==0){
+            delay_ms(150);
+            flagMenu=4;
+            break;
+            //goto countDown;
+        }
+        if(m==-1){
+            m=0;
+        }
+    } 
+    
+    //set_jam:        
+    while(flagMenu==3){
+        lcd_clear();
+        itoaVarSet();
+        tampilan();
+        lcd_gotoxy(0,1);
+        lcd_putsf("set jam");
+        delay_ms(100);
+        if(PINB.0==0){
+            delay_ms(150);
+            j=j+1;
+        }
+        if(PINB.1==0){
+        delay_ms(150);
+        j=j-1;
+        }
+        if(PINB.2==0){
+           delay_ms(150);
+            flagMenu=1;
+            break;
+        //goto set_detik;
+        }
+        if(PINB.3==0){
+            delay_ms(150);
+            flagMenu=4;
+            break;
+            //goto countDown;
+        }
+        if(j==-1){
+            j=0;
+            }
+    }
+    
+
+           
+    
+//countDown:
+    if(flagMenu==4){
+            jam=j;            // memasukkan variabel setting ke variabel intterupt untuk dihitung mundur
+            menit=m;          // penggunaan interupt untuk mendapatkan satu detik sebenarnya
+            detik=d;            
+            
+         while(1){           // looping countdown
+                          
+            lcd_clear();
+            itoaVarCount();   // mengubah nilai variabel intterupt timer untuk didisplay
+            tampilan();
+            
+            lcd_gotoxy(0,1);           
+            lcd_putsf(" >> Countdown");     // pemberitahuan menu countdown
+            delay_ms(500);
+            
+            if(jam==0 && menit==0 && detik==0){     // ketika sudah habis hitung mundurnya
+                lcd_clear();
+                itoaVarCount();
+                tampilan();         // menampilkan nilai jam menit detik yang telah bernilai nol
+                
+                lcd_gotoxy(0,1);
+                lcd_putsf("  Waktu Habis");
+                delay_ms(50);
+                PORTA.0=1;          // menyalakan sinyal pada port a.0 menuju rangkaian relay      
+                delay_ms(3000);
+                flagMenu=5;         // menuju menu minta input lagi
+                break;
+                //goto mintaLagi;
+            } 
+         }
+    }
+
+//mintaLagi:
+    while(flagMenu==5){
+        lcd_clear(); 
+        lcd_gotoxy(0,0);
+        lcd_putsf("    Tekan B1");
+        lcd_gotoxy(0,1);
+        lcd_putsf("  untuk lanjut");
+        delay_ms(100);
+        if(PINB.1==0){             // menunggu user untuk mengulangi program dengan menekan tombol b.1
+            delay_ms(100);
+            flagMenu=0;
+            break;
+            //goto setTimer;
+        }
+    }
+}    
+    
+
+
+
+
 
 // Timer2 output compare interrupt service routine
 interrupt [TIM2_COMP] void timer2_comp_isr(void)
 {
 // Place your code here
-
+TCNT2=0;  // mereset TCNT untuk dicompare lagi
+        if(++cekTimer==10000){ // 10 ribu kali untuk mendapatkan satu detik
+             detik--;          // detik decrement seterusnya sampai jam
+             cekTimer=0;
+             if(detik==-1){
+                menit--;
+                detik=59;
+                if(menit==-1){
+                    jam--;
+                    menit=59;
+                }
+             }              
+        }            
 }
-
-// Declare your global variables here
 
 void main(void)
 {
@@ -47,16 +265,16 @@ void main(void)
 
 // Input/Output Ports initialization
 // Port A initialization
-// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=Out 
-// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=0 
+// Func7=In Func6=In Func5=In Func4=In Func3=Out Func2=Out Func1=Out Func0=Out 
+// State7=T State6=T State5=T State4=T State3=0 State2=0 State1=0 State0=0 
 PORTA=0x00;
-DDRA=0x01;
+DDRA=0x0F;
 
 // Port B initialization
-// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
-// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+// Func7=Out Func6=Out Func5=Out Func4=Out Func3=In Func2=In Func1=In Func0=In 
+// State7=0 State6=0 State5=0 State4=0 State3=T State2=T State1=T State0=T 
 PORTB=0x00;
-DDRB=0x00;
+DDRB=0xF0;
 
 // Port C initialization
 // Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
@@ -160,9 +378,18 @@ lcd_init(16);
 // Global enable interrupts
 #asm("sei")
 
+// Kode Utama program
+
+PORTA.0=0;                      // meniadakan sinyal pada port a.0
+lcd_gotoxy(2,0);
+lcd_putsf("Saklar Timer");      // display judul alat
+lcd_gotoxy(4,1);
+lcd_putsf("Otomatis");
+delay_ms(1500); 
+
 while (1)
       {
       // Place your code here
-
+      menu();                // endless loop dengan memanggil fungsi menu
       }
 }
